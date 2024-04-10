f9140b8689d6d7b932539b598f372ea74db79109
unknown <abhinaykatta97@gmail.com>
2024-04-10 16:21:26 +0530

    added few more tests

--- src/components/register/Register.test.jsx
+++ src/components/register/Register.test.jsx
@@ -5,20 +5,43 @@ import userEvent from "@testing-library/user-event";
 describe("Register component", () => {
   it("should render Register component correctly", () => {
     render(<Register />);
-    const element = screen.getByRole("heading", { level: 2 });
+    const element = screen.getByRole("heading", {
+      level: 2,
+    });
     expect(element).toBeInTheDocument();
-    // element.map((item) => expect(item).toBeInTheDocument());
   });
-  it("should show error when clicked without input fields", async () => {
+
+  it("should test for presence of subheading in the component", () => {
     render(<Register />);
-    const button = screen.getByRole("button");
-    await userEvent.click(button);
+    const element = screen.getByRole("heading", {
+      name: /please enter your details below to register yourself\./i,
+    });
+    expect(element).toBeInTheDocument();
+  });
+
+  it("should show error message when all the fields are not entered", async () => {
+    render(<Register />);
+    const buttonElement = screen.getByRole("button", {
+      name: /register/i,
+    });
+    await userEvent.click(buttonElement);
     const alertElement = screen.getByRole("alert");
     expect(alertElement).toBeInTheDocument();
   });
+
   it("should not show any error message when the component is loaded", () => {
     render(<Register />);
     const alertElement = screen.queryByRole("alert");
     expect(alertElement).not.toBeInTheDocument();
+  });
+
+  it("should show success message when the registration is successful.", async () => {
+    render(<Register />);
+    const buttonElement = screen.getByRole("button", {
+      name: /register/i,
+    });
+    await userEvent.click(buttonElement);
+    const alertElement = screen.getByRole("alert");
+    expect(alertElement).toBeInTheDocument();
   });
 });
